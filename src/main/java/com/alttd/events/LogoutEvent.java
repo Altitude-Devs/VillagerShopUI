6fd575dc2e09712613afb6f4a3ccffb88ec85e0e
Teriuihi <teriuihi@alttd.com>
2021-12-22 21:07:40 +0100

    Remove user from GUIByUUID list

--- src/main/java/com/alttd/events/LogoutEvent.java
+++ src/main/java/com/alttd/events/LogoutEvent.java
@@ -1,14 +1,20 @@
 package com.alttd.events;
 
+import com.alttd.GUI.GUI;
 import com.alttd.objects.EconUser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.UUID;
+
 public class LogoutEvent implements Listener {
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        EconUser.getUser(event.getPlayer().getUniqueId()).syncPoints();
-        EconUser.removeUser(event.getPlayer().getUniqueId());
+        UUID uuid = event.getPlayer().getUniqueId();
+
+        EconUser.getUser(uuid).syncPoints();
+        EconUser.removeUser(uuid);
+        GUI.GUIByUUID.remove(uuid);
     }
 }
