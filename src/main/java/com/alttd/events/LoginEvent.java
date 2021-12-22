68978ad13c3dc4c778f9bc04320c8ff95f0dde59
Teriuihi <teriuihi@alttd.com>
2021-12-22 21:12:02 +0100

    Made login async

--- src/main/java/com/alttd/events/LoginEvent.java
+++ src/main/java/com/alttd/events/LoginEvent.java
@@ -1,20 +1,27 @@
 package com.alttd.events;
 
+import com.alttd.VillagerUI;
 import com.alttd.database.Queries;
 import com.alttd.objects.EconUser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.UUID;
 
 public class LoginEvent implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        UUID uuid = event.getPlayer().getUniqueId();
-        EconUser user = EconUser.getUser(uuid);
-        int minutes = Queries.getMinutesSinceUpdated(uuid);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                UUID uuid = event.getPlayer().getUniqueId();
+                EconUser user = EconUser.getUser(uuid);
+                int minutes = Queries.getMinutesSinceUpdated(uuid);
 
-        user.removePoints(minutes * 2);
+                user.removePoints(minutes * 2);
+            }
+        }.runTask(VillagerUI.getInstance());
     }
 }
