c1a13c66745f8dbca298e9b66cae44705dd42699
Teriuihi <teriuihi@alttd.com>
2022-01-04 00:23:30 +0100

    Wait fuck I did need this

--- src/main/java/com/alttd/GUI/GUIListener.java
+++ src/main/java/com/alttd/GUI/GUIListener.java
@@ -3,34 +3,35 @@ package com.alttd.GUI;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.TradeSelectEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class GUIListener implements Listener {
 
-//    /**
-//     * Handles clicking inside a gui
-//     * @param event gui click event
-//     */
-//    @EventHandler
-//    public void onClick(InventoryClickEvent event){
-//        if (!(event.getWhoClicked() instanceof Player player)){
-//            return;
-//        }
-//
-//        GUI gui = GUI.GUIByUUID.get(player.getUniqueId());
-//        if (gui == null || gui.getInventory() == null)
-//            return;
-//        if (!gui.getInventory().equals(event.getInventory()))
-//            return;
-//        event.setCancelled(true);
-//        GUIAction action = gui.getAction(event.getSlot());
-//
-//        if (action != null){
-//            action.click(player);
-//        }
-//    }
+    /**
+     * Handles clicking inside a gui
+     * @param event gui click event
+     */
+    @EventHandler
+    public void onClick(InventoryClickEvent event){
+        if (!(event.getWhoClicked() instanceof Player player)){
+            return;
+        }
+
+        GUI gui = GUI.GUIByUUID.get(player.getUniqueId());
+        if (gui == null || gui.getInventory() == null)
+            return;
+        if (!gui.getInventory().equals(event.getInventory()))
+            return;
+        event.setCancelled(true);
+        GUIAction action = gui.getAction(event.getSlot());
+
+        if (action != null){
+            action.click(player);
+        }
+    }
 
     @EventHandler
     public void onTradeSelect(TradeSelectEvent event) {
