d9be83ae626942168a2fa3d091835014328a12b4
Teriuihi <teriuihi@alttd.com>
2022-01-12 19:30:40 +0100

    Don't display - in prices/points when selling/buying

--- src/main/java/com/alttd/GUI/windows/BuyGUI.java
+++ src/main/java/com/alttd/GUI/windows/BuyGUI.java
@@ -126,7 +126,7 @@ public class BuyGUI extends GUIMerchant {
                 Template.template("amount", String.valueOf(purchase.amount())),
                 Template.template("item", StringUtils.capitalize(purchase.material().name()
                         .toLowerCase().replaceAll("_", " "))),
-                Template.template("price", "-" + String.format("%,.2f", purchase.price())),
+                Template.template("price", String.format("%,.2f", purchase.price())),
                 Template.template("points", String.valueOf(purchase.totalPointCost())),
                 Template.template("total_points", String.valueOf(newPoints)),
                 Template.template("villager_name", villagerType.getDisplayName())
